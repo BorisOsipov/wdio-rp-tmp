6c942c152a38865171ba82d05ea72cde723ff5af
Boris Osipov <osipov.boris@gmail.com>
2019-03-03 13:20:10 +0300

    Update test fixtures

--- specs/sample.spec.js
+++ specs/sample.spec.js
@@ -6,6 +6,14 @@ describe('describe 1', function() {
     expect($('.p-name').getText()).to.equals('Boris Osipov')
   });
 
+  it.skip('my test', () => { console.log(); });
+
+  it('skip it 1', function () {
+    this.skip();
+    browser.url('https://github.com/borisosipov');
+    expect($('.p-name').getText()).to.equals('Boris Osipov')
+  });
+
   it('it 1', function () {
     browser.url('https://github.com/borisosipov');
     expect($('.p-name').getText()).to.equals('Boris Osipov')
@@ -32,6 +40,24 @@ describe('describe 1', function() {
     it('it  1.2', function () {
       browser.url('https://github.com/borisosipov');
       expect($('.p-name').getText()).to.equals('Boris Osipov')
+    });
+
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((el) => {
+      it(`with failing test${el}`, function () {
+        this.retries(1);
+        browser.url('https://github.com/');
+        browser.getTitle();
+        expect(true).to.be.equal(true);
+      });
+    });
+  });
+
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((el) => {
+    it(`with failing test${el}`, function () {
+      this.retries(1);
+      browser.url('https://github.com/');
+      browser.getTitle();
+      expect(true).to.be.equal(false);
     });
   });
 });
--- specs/sample4.spec.js
+++ specs/sample4.spec.js
@@ -3,10 +3,13 @@ const reporter = require('../build/reporter');
 const fs = require('fs');
 
 describe('describe_2', function() {
+  beforeEach(function () {
+    expect(true).to.equals('Boris Osipov 2')
+  });
   it('it_2.1 @test @smoke', function () {
     browser.url('https://github.com/borisosipov');
-    reporter.sendLog('info', `******* TEST PASSED ******* `);
-    reporter.sendFile('debug', `name`, fs.readFileSync('./LICENCE'));
+    reporter.sendLog('debug', `******* TEST PASSED ******* `);
+    reporter.sendFile('debug', `name`,fs.readFileSync('./screenshot.png'));
     expect($('.p-name').getText()).to.equals('Boris Osipov 2')
   });
 
