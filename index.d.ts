921b0c042bbb08c9e3fa4b82c39361b9748be543
Boris Osipov <osipov.boris@gmail.com>
2019-03-23 18:45:03 +0300

    Fix typings and update wdio

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,10 @@
-import {LEVEL} from "./lib/constants";
-
-declare namespace WdioReportPortalReporter {
-    function sendLog(
-        level: LEVEL,
-        message: any,
-    ): void;
+import Reporter from "@wdio/reporter";
+import { LEVEL } from "./lib/constants";
+declare class ReportPortalReporter extends Reporter {
+  public static sendLog(level: LEVEL, message: any): void;
+  public static sendFile(level: LEVEL, name: string, content: any, type?: string): void;
+  public static sendLogToTest(test: any, level: LEVEL, message: any): void;
+  public static sendFileToTest(test: any, level: LEVEL, name: string, content: any, type?: string): void;
+  constructor(options: any);
 }
-
-declare module "index" {
-    export default WdioReportPortalReporter;
-}
-// level: LEVEL, message: any
+export = ReportPortalReporter;
